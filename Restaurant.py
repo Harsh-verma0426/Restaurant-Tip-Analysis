5c9cea7832cad38d6de306429b42f38ae642a227
Harsh-verma0426 <lionworld13913@gmail.com>
2025-07-14 13:27:49 +0530

    Fixed Minor Bugs

--- Restaurant.py
+++ Restaurant.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import numpy as np
 import sys
 
-df = pd.read_csv('data/Unclean_Restaurant_Data.csv')
+df = pd.read_csv(r'Restaurant-Tip-Analysis/data/Unclean_Restaurant_Data.csv')
 
 # Data Cleaning Function
 def clean_data(df):
@@ -40,74 +40,82 @@ else:
 def avg_tip_day(df_cleaned):
     avg_tip_by_day = df_cleaned.groupby('day')['tip'].mean()
     print(avg_tip_by_day.round(2))
+    plt.clf()
     plt.bar(avg_tip_by_day.index, avg_tip_by_day.values)
     plt.xlabel('Day of the Week')
     plt.ylabel('Average Tip')
     plt.title('Average Tip by Day')
-    plt.savefig('Visuals/avg_tip_by_day.png')
+    plt.savefig('Restaurant-Tip-Analysis/Visuals/avg_tip_by_day.png')
+    return "Graph Successfully Saved for Question 2"
 
-avg_tip_day(df_cleaned)
+print(avg_tip_day(df_cleaned))
 
 # Question 3. Do smokers tip differently compared to non-smokers?
 def avg_tip_by_smoker_status(df_cleaned):
     smoker_tips = df_cleaned.groupby('smoker')['tip'].mean()
-    print(smoker_tips.round(2))
+    plt.clf()
     sns.boxplot(x='smoker', y='tip', data=df_cleaned)
     plt.title("Tip Amount: Smokers vs Non-Smokers")
-    plt.savefig('Visuals/tip_amt_btw_smoker_and_non_smoker.png')
+    plt.savefig('Restaurant-Tip-Analysis/Visuals/tip_amt_btw_smoker_and_non_smoker.png')
+    return "Graph Successfully Saved for Question 3"
 
 print(avg_tip_by_smoker_status(df_cleaned))
 
 # Question 4. How does party size affect the total bill or tip amount?
 def avg_tip_and_bill_by_size(df_cleaned):
     avg_tip_by_size = df_cleaned.groupby('size')['tip'].mean()
-    print(avg_tip_by_size.round(2))
+    plt.clf()
     plt.bar(avg_tip_by_size.index, avg_tip_by_size.values)
     plt.xlabel('Party Size')
     plt.ylabel('Average Tip')
     plt.title('Average Tip by Party Size')
-    plt.savefig('Visuals/avg_tip_by_size.png')
+    plt.savefig('Restaurant-Tip-Analysis/Visuals/avg_tip_by_size.png')
+    
 
     avg_bill_by_size = df_cleaned.groupby('size')['total_bill'].mean()
-    print(avg_bill_by_size.round(2))
+    plt.clf()
     plt.bar(avg_bill_by_size.index, avg_bill_by_size.values)
     plt.xlabel('Party Size')
     plt.ylabel('Average Bill')
     plt.title('Average Bill by Party Size')
-    plt.savefig('Visuals/avg_bill_by_size.png')
+    plt.savefig('Restaurant-Tip-Analysis/Visuals/avg_bill_by_size.png')
+    return "Graph Successfully Saved for Question 4(a) and Question 4(b)"
 
 print(avg_tip_and_bill_by_size(df_cleaned))
 
 # Question 5. Is there a noticeable difference in tipping behavior during Lunch and Dinner?
 def avg_tip_by_time(df_cleaned):
     avg_tip_by_time = df_cleaned.groupby('time')['tip'].mean()
-    print(avg_tip_by_time.round(2))
+    plt.clf()
     plt.bar(avg_tip_by_time.index, avg_tip_by_time.values)
     plt.xlabel('Time of Day')
     plt.ylabel('Average Tip')
     plt.title('Average Tip by Time of Day')
-    plt.savefig('Visuals/avg_tip_by_time.png')
+    plt.savefig('Restaurant-Tip-Analysis/Visuals/avg_tip_by_time.png')
+    return "Graph Successfully Saved for Question 5"
 
 print(avg_tip_by_time(df_cleaned))
 
 # Question 6. Is there a correlation between total bill, tip amount, and party size?
 def relationship_total_bill_tip_party_size(df_cleaned):
     correlation = df_cleaned[['total_bill', 'tip', 'size']].corr()
+    plt.clf()
     sns.heatmap(correlation, annot=True, cmap='YlGnBu')
     plt.title("Correlation Between Total Bill, Tip, and Party Size")
-    plt.savefig('Visuals/correlation_total_bill_tip_party_size.png')
-    return "Graph Successfully Saved for Question 7"
+    plt.savefig('Restaurant-Tip-Analysis/Visuals/correlation_total_bill_tip_party_size.png')
+    return "Graph Successfully Saved for Question 6"
 
 print(relationship_total_bill_tip_party_size(df_cleaned))
 
 # Question 7. Which day of the week brings in the highest total bill amounts?
 def highest_total_bill_day(df_cleaned):
     Total_bill_by_day = df_cleaned.groupby('day')['total_bill'].sum()
+    plt.clf()
     plt.bar(Total_bill_by_day.index, Total_bill_by_day.values)
     plt.xlabel('Day of the Week')
     plt.ylabel('Total Bill Amount')
     plt.title('Total Bill Amount by Day of the Week')
-    plt.savefig('Visuals/total_bill_by_day.png')
+    plt.savefig('Restaurant-Tip-Analysis/Visuals/total_bill_by_day.png')
     return "Graph Successfully Saved for Question 7"
 
 print(highest_total_bill_day(df_cleaned))
